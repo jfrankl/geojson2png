5a32d18183d938a87a430fd2b8f70ce048c64bf2
jfrankl <jfrankl@azavea.com>
2020-10-16 17:38:07 -0400

    Add districts example

--- districts.js
+++ districts.js
@@ -2,8 +2,8 @@ var mapnik = require('mapnik');
 var fs = require('fs');
 
 mapnik.register_default_input_plugins();
-var map = new mapnik.Map(600, 400);
+var map = new mapnik.Map(600, 600);
 map.loadSync("data/countries-stylesheet.xml");
 map.zoomAll();
-map.renderFileSync("example2.png");
-console.log('saved map image to example2.png');
+map.renderFileSync("districts.png");
+console.log('saved map');
